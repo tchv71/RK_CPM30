4f7ad249e139840a9587d90487f06b4eab0b99bd
Dmitry <tchv71@gmail.com>
2023-06-30 13:59:56 +0300

    First palmira CP/M SD working version

---
+++ CPM/readme.txt
@@ -0,0 +1,1 @@
+Place this dir (CPM) into SD root folder
---
+++ b0FlpDmy.asm
@@ -0,0 +1,15 @@
+FlpD_I:
+FLSH_WB:
+WT_END_CMD:
+ChgDrive:
+FlpD_R:
+FlpD_W:
+	RET
+; Disk parameters
+FLP_TBL::
+RecsPerSect:	db	4
+MaxSect:	db	9	; Maximum sector number
+FullMask:	db	0fh	; Full sector mask
+SectShift:	db	2
+
+LAST_OPER:	DB	0
