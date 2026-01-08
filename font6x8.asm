0fc187796b7a92c2152a57fe5499ca7a7d209a3b
Дмитрий Цветков <tchv71@mail.ru>
2026-01-08 20:48:39 +0300

    9938 like text scrolling on PICO side

---
+++ font6x8.asm
@@ -0,0 +1,512 @@
+	;  char 0x00
+	DB	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x01
+	DB	0E0h, 0E0h, 0E0h, 0E0h, 000h, 000h, 000h, 000h
+	;  char 0x02
+	DB	01Ch, 01Ch, 01Ch, 01Ch, 000h, 000h, 000h, 000h
+	;  char 0x03
+	DB	0FCh, 0FCh, 0FCh, 0FCh, 000h, 000h, 000h, 000h
+	;  char 0x04
+	DB	000h, 000h, 000h, 000h, 01Ch, 01Ch, 01Ch, 01Ch
+	;  char 0x05
+	DB	0E0h, 0E0h, 0E0h, 0E0h, 01Ch, 01Ch, 01Ch, 01Ch
+	;  char 0x06
+	DB	01Ch, 01Ch, 01Ch, 01Ch, 01Ch, 01Ch, 01Ch, 01Ch
+	;  char 0x07
+	DB	0FCh, 0FCh, 0FCh, 0FCh, 01Ch, 01Ch, 01Ch, 01Ch
+	;  char 0x08
+	DB	078h, 0FCh, 0B4h, 0FCh, 084h, 0CCh, 0FCh, 078h
+	;  char 0x09
+	DB	030h, 030h, 0FCh, 0B4h, 030h, 030h, 048h, 084h
+	;  char 0x0a
+	DB	030h, 048h, 084h, 0CCh, 084h, 0B4h, 048h, 030h
+	;  char 0x0b
+	DB	030h, 078h, 0FCh, 030h, 030h, 030h, 030h, 030h
+	;  char 0x0c
+	DB	000h, 050h, 0F8h, 0F8h, 070h, 020h, 000h, 000h
+	;  char 0x0d
+	DB	000h, 020h, 070h, 0F8h, 070h, 020h, 000h, 000h
+	;  char 0x0e
+	DB	000h, 000h, 030h, 018h, 0FCh, 018h, 030h, 000h
+	;  char 0x0f
+	DB	030h, 030h, 030h, 030h, 030h, 0FCh, 078h, 030h
+	;  char 0x10
+	DB	000h, 000h, 000h, 000h, 0E0h, 0E0h, 0E0h, 0E0h
+	;  char 0x11
+	DB	0E0h, 0E0h, 0E0h, 0E0h, 0E0h, 0E0h, 0E0h, 0E0h
+	;  char 0x12
+	DB	01Ch, 01Ch, 01Ch, 01Ch, 0E0h, 0E0h, 0E0h, 0E0h
+	;  char 0x13
+	DB	0FCh, 0FCh, 0FCh, 0FCh, 0E0h, 0E0h, 0E0h, 0E0h
+	;  char 0x14
+	DB	000h, 000h, 000h, 000h, 0FCh, 0FCh, 0FCh, 0FCh
+	;  char 0x15
+	DB	0E0h, 0E0h, 0E0h, 0E0h, 0FCh, 0FCh, 0FCh, 0FCh
+	;  char 0x16
+	DB	01Ch, 01Ch, 01Ch, 01Ch, 0FCh, 0FCh, 0FCh, 0FCh
+	;  char 0x17
+	DB	0FCh, 0FCh, 0FCh, 0FCh, 0FCh, 0FCh, 0FCh, 0FCh
+	;  char 0x18
+	DB	078h, 030h, 0B4h, 0FCh, 0B4h, 030h, 030h, 078h
+	;  char 0x19
+	DB	000h, 030h, 078h, 0FCh, 0FCh, 0B4h, 030h, 078h
+	;  char 0x1a
+	DB	000h, 000h, 018h, 018h, 000h, 000h, 000h, 000h
+	;  char 0x1b
+	DB	030h, 030h, 030h, 030h, 030h, 030h, 030h, 030h
+	;  char 0x1c
+	DB	000h, 000h, 000h, 0FCh, 0FCh, 000h, 000h, 000h
+	;  char 0x1d
+	DB	000h, 000h, 030h, 060h, 0FCh, 060h, 030h, 000h
+	;  char 0x1e
+	DB	0E0h, 080h, 080h, 09Ch, 0F4h, 014h, 014h, 014h
+	;  char 0x1f
+	DB	000h, 038h, 06Ch, 044h, 06Ch, 038h, 000h, 000h
+	;  char 0x20
+	DB	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x21
+	DB	020h, 020h, 020h, 020h, 000h, 000h, 020h, 000h
+	;  char 0x22
+	DB	050h, 050h, 050h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x23
+	DB	050h, 050h, 0F8h, 050h, 0F8h, 050h, 050h, 000h
+	;  char 0x24
+	DB	020h, 078h, 0A0h, 070h, 028h, 0F0h, 020h, 000h
+	;  char 0x25
+	DB	0C0h, 0C8h, 010h, 020h, 040h, 098h, 018h, 000h
+	;  char 0x26
+	DB	040h, 0A0h, 040h, 0A8h, 090h, 098h, 060h, 000h
+	;  char 0x27
+	DB	010h, 020h, 040h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x28
+	DB	010h, 020h, 040h, 040h, 040h, 020h, 010h, 000h
+	;  char 0x29
+	DB	040h, 020h, 010h, 010h, 010h, 020h, 040h, 000h
+	;  char 0x2a
+	DB	088h, 050h, 020h, 0F8h, 020h, 050h, 088h, 000h
+	;  char 0x2b
+	DB	000h, 020h, 020h, 0F8h, 020h, 020h, 000h, 000h
+	;  char 0x2c
+	DB	000h, 000h, 000h, 000h, 000h, 020h, 020h, 040h
+	;  char 0x2d
+	DB	000h, 000h, 000h, 078h, 000h, 000h, 000h, 000h
+	;  char 0x2e
+	DB	000h, 000h, 000h, 000h, 000h, 060h, 060h, 000h
+	;  char 0x2f
+	DB	000h, 000h, 008h, 010h, 020h, 040h, 080h, 000h
+	;  char 0x30
+	DB	070h, 088h, 098h, 0A8h, 0C8h, 088h, 070h, 000h
+	;  char 0x31
+	DB	020h, 060h, 0A0h, 020h, 020h, 020h, 0F8h, 000h
+	;  char 0x32
+	DB	070h, 088h, 008h, 010h, 060h, 080h, 0F8h, 000h
+	;  char 0x33
+	DB	070h, 088h, 008h, 030h, 008h, 088h, 070h, 000h
+	;  char 0x34
+	DB	010h, 030h, 050h, 090h, 0F8h, 010h, 010h, 000h
+	;  char 0x35
+	DB	0F8h, 080h, 0E0h, 010h, 008h, 010h, 0E0h, 000h
+	;  char 0x36
+	DB	030h, 040h, 080h, 0F0h, 088h, 088h, 070h, 000h
+	;  char 0x37
+	DB	0F8h, 088h, 010h, 020h, 020h, 020h, 020h, 000h
+	;  char 0x38
+	DB	070h, 088h, 088h, 070h, 088h, 088h, 070h, 000h
+	;  char 0x39
+	DB	070h, 088h, 088h, 078h, 008h, 010h, 060h, 000h
+	;  char 0x3a
+	DB	000h, 000h, 020h, 000h, 000h, 020h, 000h, 000h
+	;  char 0x3b
+	DB	000h, 000h, 020h, 000h, 000h, 020h, 020h, 040h
+	;  char 0x3c
+	DB	018h, 030h, 060h, 0C0h, 060h, 030h, 018h, 000h
+	;  char 0x3d
+	DB	000h, 000h, 0F8h, 000h, 0F8h, 000h, 000h, 000h
+	;  char 0x3e
+	DB	0C0h, 060h, 030h, 018h, 030h, 060h, 0C0h, 000h
+	;  char 0x3f
+	DB	070h, 088h, 008h, 010h, 020h, 000h, 020h, 000h
+	;  char 0x40
+	DB	070h, 088h, 008h, 068h, 0A8h, 0A8h, 070h, 000h
+	;  char 0x41
+	DB	020h, 050h, 088h, 088h, 0F8h, 088h, 088h, 000h
+	;  char 0x42
+	DB	0F0h, 048h, 048h, 070h, 048h, 048h, 0F0h, 000h
+	;  char 0x43
+	DB	030h, 048h, 080h, 080h, 080h, 048h, 030h, 000h
+	;  char 0x44
+	DB	0E0h, 050h, 048h, 048h, 048h, 050h, 0E0h, 000h
+	;  char 0x45
+	DB	0F8h, 080h, 080h, 0F0h, 080h, 080h, 0F8h, 000h
+	;  char 0x46
+	DB	0F8h, 080h, 080h, 0F0h, 080h, 080h, 080h, 000h
+	;  char 0x47
+	DB	070h, 088h, 080h, 0B8h, 088h, 088h, 070h, 000h
+	;  char 0x48
+	DB	088h, 088h, 088h, 0F8h, 088h, 088h, 088h, 000h
+	;  char 0x49
+	DB	070h, 020h, 020h, 020h, 020h, 020h, 070h, 000h
+	;  char 0x4a
+	DB	038h, 010h, 010h, 010h, 090h, 090h, 060h, 000h
+	;  char 0x4b
+	DB	088h, 090h, 0A0h, 0C0h, 0A0h, 090h, 088h, 000h
+	;  char 0x4c
+	DB	080h, 080h, 080h, 080h, 080h, 080h, 0F8h, 000h
+	;  char 0x4d
+	DB	088h, 0D8h, 0A8h, 0A8h, 088h, 088h, 088h, 000h
+	;  char 0x4e
+	DB	088h, 0C8h, 0C8h, 0A8h, 098h, 098h, 088h, 000h
+	;  char 0x4f
+	DB	070h, 088h, 088h, 088h, 088h, 088h, 070h, 000h
+	;  char 0x50
+	DB	0F0h, 088h, 088h, 0F0h, 080h, 080h, 080h, 000h
+	;  char 0x51
+	DB	070h, 088h, 088h, 088h, 0A8h, 090h, 068h, 000h
+	;  char 0x52
+	DB	0F0h, 088h, 088h, 0F0h, 0A0h, 090h, 088h, 000h
+	;  char 0x53
+	DB	070h, 088h, 080h, 070h, 008h, 088h, 070h, 000h
+	;  char 0x54
+	DB	0F8h, 020h, 020h, 020h, 020h, 020h, 020h, 000h
+	;  char 0x55
+	DB	088h, 088h, 088h, 088h, 088h, 088h, 070h, 000h
+	;  char 0x56
+	DB	088h, 088h, 088h, 088h, 050h, 050h, 020h, 000h
+	;  char 0x57
+	DB	088h, 088h, 088h, 0A8h, 0A8h, 0D8h, 088h, 000h
+	;  char 0x58
+	DB	088h, 088h, 050h, 020h, 050h, 088h, 088h, 000h
+	;  char 0x59
+	DB	088h, 088h, 088h, 070h, 020h, 020h, 020h, 000h
+	;  char 0x5a
+	DB	0F8h, 008h, 010h, 020h, 040h, 080h, 0F8h, 000h
+	;  char 0x5b
+	DB	070h, 040h, 040h, 040h, 040h, 040h, 070h, 000h
+	;  char 0x5c
+	DB	000h, 000h, 080h, 040h, 020h, 010h, 008h, 000h
+	;  char 0x5d
+	DB	070h, 010h, 010h, 010h, 010h, 010h, 070h, 000h
+	;  char 0x5e
+	DB	020h, 050h, 088h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x5f
+	DB	000h, 000h, 000h, 000h, 000h, 000h, 0F8h, 000h
+	;  char 0x60
+	DB	040h, 020h, 010h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x61
+	DB	000h, 000h, 070h, 008h, 078h, 088h, 078h, 000h
+	;  char 0x62
+	DB	080h, 080h, 0B0h, 0C8h, 088h, 0C8h, 0B0h, 000h
+	;  char 0x63
+	DB	000h, 000h, 070h, 088h, 080h, 088h, 070h, 000h
+	;  char 0x64
+	DB	008h, 008h, 068h, 098h, 088h, 098h, 068h, 000h
+	;  char 0x65
+	DB	000h, 000h, 070h, 088h, 0F8h, 080h, 070h, 000h
+	;  char 0x66
+	DB	010h, 028h, 020h, 0F8h, 020h, 020h, 020h, 000h
+	;  char 0x67
+	DB	000h, 000h, 068h, 098h, 098h, 068h, 008h, 070h
+	;  char 0x68
+	DB	080h, 080h, 0F0h, 088h, 088h, 088h, 088h, 000h
+	;  char 0x69
+	DB	020h, 000h, 060h, 020h, 020h, 020h, 070h, 000h
+	;  char 0x6a
+	DB	010h, 000h, 030h, 010h, 010h, 010h, 090h, 060h
+	;  char 0x6b
+	DB	040h, 040h, 048h, 050h, 060h, 050h, 048h, 000h
+	;  char 0x6c
+	DB	060h, 020h, 020h, 020h, 020h, 020h, 070h, 000h
+	;  char 0x6d
+	DB	000h, 000h, 0D0h, 0A8h, 0A8h, 0A8h, 0A8h, 000h
+	;  char 0x6e
+	DB	000h, 000h, 0B0h, 0C8h, 088h, 088h, 088h, 000h
+	;  char 0x6f
+	DB	000h, 000h, 070h, 088h, 088h, 088h, 070h, 000h
+	;  char 0x70
+	DB	000h, 000h, 0B0h, 0C8h, 0C8h, 0B0h, 080h, 080h
+	;  char 0x71
+	DB	000h, 000h, 068h, 098h, 098h, 068h, 008h, 008h
+	;  char 0x72
+	DB	000h, 000h, 0B0h, 0C8h, 080h, 080h, 080h, 000h
+	;  char 0x73
+	DB	000h, 000h, 078h, 080h, 0F0h, 008h, 0F0h, 000h
+	;  char 0x74
+	DB	040h, 040h, 0F0h, 040h, 040h, 048h, 030h, 000h
+	;  char 0x75
+	DB	000h, 000h, 090h, 090h, 090h, 090h, 068h, 000h
+	;  char 0x76
+	DB	000h, 000h, 088h, 088h, 088h, 050h, 020h, 000h
+	;  char 0x77
+	DB	000h, 000h, 088h, 0A8h, 0A8h, 0A8h, 050h, 000h
+	;  char 0x78
+	DB	000h, 000h, 088h, 050h, 020h, 050h, 088h, 000h
+	;  char 0x79
+	DB	000h, 000h, 088h, 088h, 098h, 068h, 008h, 070h
+	;  char 0x7a
+	DB	000h, 000h, 0F8h, 010h, 020h, 040h, 0F8h, 000h
+	;  char 0x7b
+	DB	018h, 020h, 020h, 040h, 020h, 020h, 018h, 000h
+	;  char 0x7c
+	DB	020h, 020h, 020h, 000h, 020h, 020h, 020h, 000h
+	;  char 0x7d
+	DB	0C0h, 020h, 020h, 010h, 020h, 020h, 0C0h, 000h
+	;  char 0x7e
+	DB	040h, 0A8h, 010h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x7f
+	DB	000h, 000h, 020h, 050h, 0F8h, 000h, 000h, 000h
+	;  char 0x80
+	DB	000h, 000h, 000h, 000h, 0FCh, 000h, 000h, 000h
+	;  char 0x81
+	DB	030h, 030h, 030h, 030h, 030h, 030h, 030h, 030h
+	;  char 0x82
+	DB	000h, 000h, 000h, 000h, 03Ch, 030h, 030h, 030h
+	;  char 0x83
+	DB	000h, 000h, 000h, 000h, 0F0h, 030h, 030h, 030h
+	;  char 0x84
+	DB	030h, 030h, 03Ch, 000h, 000h, 000h, 000h, 000h
+	;  char 0x85
+	DB	030h, 030h, 0F0h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x86
+	DB	030h, 030h, 030h, 030h, 03Ch, 030h, 030h, 030h
+	;  char 0x87
+	DB	030h, 030h, 030h, 030h, 0F0h, 030h, 030h, 030h
+	;  char 0x88
+	DB	000h, 000h, 000h, 000h, 0FCh, 030h, 030h, 030h
+	;  char 0x89
+	DB	030h, 030h, 030h, 030h, 0FCh, 000h, 000h, 000h
+	;  char 0x8a
+	DB	030h, 030h, 030h, 030h, 0FCh, 030h, 030h, 030h
+	;  char 0x8b
+	DB	03Ch, 00Ch, 014h, 024h, 060h, 090h, 090h, 060h
+	;  char 0x8c
+	DB	0F0h, 0F0h, 0F0h, 0F0h, 00Fh, 00Fh, 00Fh, 00Fh
+	;  char 0x8d
+	DB	000h, 000h, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh, 0FFh
+	;  char 0x8e
+	DB	0FFh, 0FFh, 000h, 000h, 000h, 000h, 000h, 000h
+	;  char 0x8f
+	DB	000h, 000h, 000h, 03Ch, 03Ch, 000h, 000h, 000h
+	;  char 0x90
+	DB	008h, 022h, 008h, 022h, 008h, 022h, 008h, 022h
+	;  char 0x91
+	DB	015h, 02Ah, 015h, 02Ah, 015h, 02Ah, 015h, 02Ah
+	;  char 0x92
+	DB	02Eh, 03Bh, 02Eh, 01Dh, 02Eh, 03Bh, 02Eh, 01Dh
+	;  char 0x93
+	DB	080h, 0C0h, 0E0h, 0F0h, 0E0h, 0C0h, 080h, 000h
+	;  char 0x94
+	DB	001h, 003h, 007h, 00Fh, 007h, 003h, 001h, 000h
+	;  char 0x95
+	DB	030h, 078h, 0FCh, 030h, 030h, 0FCh, 078h, 030h
+	;  char 0x96
+	DB	078h, 0C0h, 070h, 0D8h, 0D8h, 070h, 018h, 0F0h
+	;  char 0x97
+	DB	078h, 0C0h, 070h, 0D8h, 0D8h, 070h, 018h, 0F0h
+	;  char 0x98
+	DB	000h, 06Ch, 06Ch, 06Ch, 06Ch, 06Ch, 000h, 06Ch
+	;  char 0x99
+	DB	000h, 000h, 000h, 078h, 078h, 078h, 000h, 000h
+	;  char 0x9a
+	DB	030h, 078h, 0FCh, 030h, 0FCh, 078h, 030h, 0FCh
+	;  char 0x9b
+	DB	000h, 000h, 000h, 0C0h, 0C0h, 0FCh, 000h, 000h
+	;  char 0x9c
+	DB	0FFh, 07Eh, 03Ch, 018h, 018h, 03Ch, 07Eh, 0FFh
+	;  char 0x9d
+	DB	010h, 038h, 07Ch, 0FEh, 000h, 000h, 000h, 000h
+	;  char 0x9e
+	DB	0FEh, 07Ch, 038h, 010h, 000h, 000h, 000h, 000h
+	;  char 0x9f
+	DB	0E0h, 020h, 020h, 038h, 024h, 024h, 038h, 000h
+	;  char 0xa0
+	DB	000h, 000h, 0FCh, 000h, 0FCh, 000h, 000h, 000h
+	;  char 0xa1
+	DB	048h, 048h, 048h, 048h, 048h, 048h, 048h, 048h
+	;  char 0xa2
+	DB	000h, 000h, 03Ch, 020h, 03Ch, 030h, 030h, 030h
+	;  char 0xa3
+	DB	050h, 000h, 070h, 088h, 0F8h, 080h, 070h, 000h
+	;  char 0xa4
+	DB	000h, 000h, 000h, 000h, 07Ch, 048h, 048h, 048h
+	;  char 0xa5
+	DB	000h, 000h, 07Ch, 040h, 04Ch, 048h, 048h, 048h
+	;  char 0xa6
+	DB	000h, 000h, 0F0h, 010h, 0F0h, 030h, 030h, 030h
+	;  char 0xa7
+	DB	000h, 000h, 000h, 000h, 0F8h, 048h, 048h, 048h
+	;  char 0xa8
+	DB	000h, 000h, 0F8h, 008h, 0C8h, 048h, 048h, 048h
+	;  char 0xa9
+	DB	030h, 030h, 03Ch, 020h, 03Ch, 000h, 000h, 000h
+	;  char 0xaa
+	DB	048h, 048h, 048h, 048h, 07Ch, 000h, 000h, 000h
+	;  char 0xab
+	DB	048h, 048h, 04Ch, 040h, 07Ch, 000h, 000h, 000h
+	;  char 0xac
+	DB	030h, 030h, 0F0h, 010h, 0F0h, 000h, 000h, 000h
+	;  char 0xad
+	DB	048h, 048h, 048h, 048h, 0F8h, 000h, 000h, 000h
+	;  char 0xae
+	DB	048h, 048h, 0C8h, 008h, 0F8h, 000h, 000h, 000h
+	;  char 0xaf
+	DB	00Ch, 00Ch, 00Fh, 00Ch, 00Fh, 00Ch, 00Ch, 00Ch
+	;  char 0xb0
+	DB	048h, 048h, 048h, 048h, 04Ch, 048h, 048h, 048h
+	;  char 0xb1
+	DB	048h, 048h, 04Ch, 040h, 04Ch, 048h, 048h, 048h
+	;  char 0xb2
+	DB	030h, 030h, 0F0h, 030h, 0F0h, 030h, 030h, 030h
+	;  char 0xb3
+	DB	050h, 0F8h, 080h, 0F0h, 080h, 080h, 0F8h, 000h
+	;  char 0xb4
+	DB	048h, 048h, 048h, 048h, 0C8h, 048h, 048h, 048h
+	;  char 0xb5
+	DB	048h, 048h, 0C8h, 008h, 0C8h, 048h, 048h, 048h
+	;  char 0xb6
+	DB	000h, 000h, 0FCh, 000h, 0FCh, 030h, 030h, 030h
+	;  char 0xb7
+	DB	000h, 000h, 000h, 000h, 0FCh, 048h, 048h, 048h
+	;  char 0xb8
+	DB	000h, 000h, 0FCh, 000h, 0CCh, 048h, 048h, 048h
+	;  char 0xb9
+	DB	030h, 030h, 0FCh, 000h, 0FCh, 000h, 000h, 000h
+	;  char 0xba
+	DB	048h, 048h, 0CCh, 000h, 0FCh, 000h, 000h, 000h
+	;  char 0xbb
+	DB	048h, 048h, 048h, 048h, 0FCh, 000h, 000h, 000h
+	;  char 0xbc
+	DB	030h, 030h, 0FCh, 030h, 0FCh, 030h, 030h, 030h
+	;  char 0xbd
+	DB	048h, 048h, 048h, 048h, 0FCh, 048h, 048h, 048h
+	;  char 0xbe
+	DB	048h, 048h, 0CCh, 000h, 0CCh, 048h, 048h, 048h
+	;  char 0xbf
+	DB	030h, 048h, 0B4h, 0A4h, 0B4h, 048h, 030h, 000h
+	;  char 0xc0
+	DB	000h, 000h, 090h, 0A8h, 0E8h, 0A8h, 090h, 000h
+	;  char 0xc1
+	DB	000h, 000h, 060h, 010h, 070h, 090h, 068h, 000h
+	;  char 0xc2
+	DB	000h, 000h, 0F0h, 080h, 0F0h, 088h, 0F0h, 000h
+	;  char 0xc3
+	DB	000h, 000h, 090h, 090h, 090h, 0F8h, 008h, 000h
+	;  char 0xc4
+	DB	000h, 000h, 030h, 050h, 050h, 070h, 088h, 000h
+	;  char 0xc5
+	DB	000h, 000h, 070h, 088h, 0F8h, 080h, 070h, 000h
+	;  char 0xc6
+	DB	000h, 020h, 070h, 0A8h, 0A8h, 070h, 020h, 000h
+	;  char 0xc7
+	DB	000h, 000h, 078h, 048h, 040h, 040h, 040h, 000h
+	;  char 0xc8
+	DB	000h, 000h, 088h, 050h, 020h, 050h, 088h, 000h
+	;  char 0xc9
+	DB	000h, 000h, 088h, 098h, 0A8h, 0C8h, 088h, 000h
+	;  char 0xca
+	DB	000h, 050h, 020h, 000h, 098h, 0A8h, 0C8h, 000h
+	;  char 0xcb
+	DB	000h, 000h, 090h, 0A0h, 0C0h, 0A0h, 090h, 000h
+	;  char 0xcc
+	DB	000h, 000h, 038h, 028h, 028h, 048h, 088h, 000h
+	;  char 0xcd
+	DB	000h, 000h, 088h, 0D8h, 0A8h, 088h, 088h, 000h
+	;  char 0xce
+	DB	000h, 000h, 088h, 088h, 0F8h, 088h, 088h, 000h
+	;  char 0xcf
+	DB	000h, 000h, 070h, 088h, 088h, 088h, 070h, 000h
+	;  char 0xd0
+	DB	000h, 000h, 078h, 048h, 048h, 048h, 048h, 000h
+	;  char 0xd1
+	DB	000h, 000h, 078h, 088h, 078h, 028h, 048h, 000h
+	;  char 0xd2
+	DB	000h, 000h, 0F0h, 088h, 0F0h, 080h, 080h, 000h
+	;  char 0xd3
+	DB	000h, 000h, 078h, 080h, 080h, 080h, 078h, 000h
+	;  char 0xd4
+	DB	000h, 000h, 0F8h, 020h, 020h, 020h, 020h, 000h
+	;  char 0xd5
+	DB	000h, 000h, 088h, 050h, 020h, 040h, 080h, 000h
+	;  char 0xd6
+	DB	000h, 000h, 0A8h, 070h, 020h, 070h, 0A8h, 000h
+	;  char 0xd7
+	DB	000h, 000h, 0F0h, 048h, 070h, 048h, 0F0h, 000h
+	;  char 0xd8
+	DB	000h, 000h, 040h, 040h, 070h, 048h, 070h, 000h
+	;  char 0xd9
+	DB	000h, 000h, 088h, 088h, 0C8h, 0A8h, 0C8h, 000h
+	;  char 0xda
+	DB	000h, 000h, 0F0h, 008h, 070h, 008h, 0F0h, 000h
+	;  char 0xdb
+	DB	000h, 000h, 0A8h, 0A8h, 0A8h, 0A8h, 0F8h, 000h
+	;  char 0xdc
+	DB	000h, 000h, 070h, 088h, 038h, 088h, 070h, 000h
+	;  char 0xdd
+	DB	000h, 000h, 0A8h, 0A8h, 0A8h, 0F8h, 008h, 000h
+	;  char 0xde
+	DB	000h, 000h, 048h, 048h, 078h, 008h, 008h, 000h
+	;  char 0xdf
+	DB	000h, 000h, 0C0h, 040h, 070h, 048h, 070h, 000h
+	;  char 0xe0
+	DB	090h, 0A8h, 0A8h, 0E8h, 0A8h, 0A8h, 090h, 000h
+	;  char 0xe1
+	DB	020h, 050h, 088h, 088h, 0F8h, 088h, 088h, 000h
+	;  char 0xe2
+	DB	0F8h, 088h, 080h, 0F0h, 088h, 088h, 0F0h, 000h
+	;  char 0xe3
+	DB	090h, 090h, 090h, 090h, 090h, 0F8h, 008h, 000h
+	;  char 0xe4
+	DB	038h, 028h, 028h, 048h, 048h, 0F8h, 088h, 000h
+	;  char 0xe5
+	DB	0F8h, 080h, 080h, 0F0h, 080h, 080h, 0F8h, 000h
+	;  char 0xe6
+	DB	020h, 070h, 0A8h, 0A8h, 0A8h, 070h, 020h, 000h
+	;  char 0xe7
+	DB	0F8h, 088h, 088h, 080h, 080h, 080h, 080h, 000h
+	;  char 0xe8
+	DB	088h, 088h, 050h, 020h, 050h, 088h, 088h, 000h
+	;  char 0xe9
+	DB	088h, 088h, 098h, 0A8h, 0C8h, 088h, 088h, 000h
+	;  char 0xea
+	DB	050h, 020h, 088h, 098h, 0A8h, 0C8h, 088h, 000h
+	;  char 0xeb
+	DB	088h, 090h, 0A0h, 0C0h, 0A0h, 090h, 088h, 000h
+	;  char 0xec
+	DB	018h, 028h, 048h, 048h, 048h, 048h, 088h, 000h
+	;  char 0xed
+	DB	088h, 0D8h, 0A8h, 0A8h, 088h, 088h, 088h, 000h
+	;  char 0xee
+	DB	088h, 088h, 088h, 0F8h, 088h, 088h, 088h, 000h
+	;  char 0xef
+	DB	070h, 088h, 088h, 088h, 088h, 088h, 070h, 000h
+	;  char 0xf0
+	DB	0F8h, 088h, 088h, 088h, 088h, 088h, 088h, 000h
+	;  char 0xf1
+	DB	078h, 088h, 088h, 078h, 028h, 048h, 088h, 000h
+	;  char 0xf2
+	DB	0F0h, 088h, 088h, 0F0h, 080h, 080h, 080h, 000h
+	;  char 0xf3
+	DB	070h, 088h, 080h, 080h, 080h, 088h, 070h, 000h
+	;  char 0xf4
+	DB	0F8h, 020h, 020h, 020h, 020h, 020h, 020h, 000h
+	;  char 0xf5
+	DB	088h, 088h, 088h, 050h, 020h, 040h, 080h, 000h
+	;  char 0xf6
+	DB	0A8h, 0A8h, 070h, 020h, 070h, 0A8h, 0A8h, 000h
+	;  char 0xf7
+	DB	0F0h, 048h, 048h, 070h, 048h, 048h, 0F0h, 000h
+	;  char 0xf8
+	DB	080h, 080h, 080h, 0F0h, 088h, 088h, 0F0h, 000h
+	;  char 0xf9
+	DB	088h, 088h, 088h, 0C8h, 0A8h, 0A8h, 0C8h, 000h
+	;  char 0xfa
+	DB	0F0h, 008h, 008h, 030h, 008h, 008h, 0F0h, 000h
+	;  char 0xfb
+	DB	0A8h, 0A8h, 0A8h, 0A8h, 0A8h, 0A8h, 0F8h, 000h
+	;  char 0xfc
+	DB	070h, 088h, 008h, 078h, 008h, 088h, 070h, 000h
+	;  char 0xfd
+	DB	0A8h, 0A8h, 0A8h, 0A8h, 0A8h, 0F8h, 008h, 000h
+	;  char 0xfe
+	DB	088h, 088h, 088h, 088h, 078h, 008h, 008h, 000h
+	;  char 0xff
+	DB	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
